c4d99220851c0a95d1d82d6d50406dcda0edc5b2
YuraBuchak <115108617+YuraBuchak@users.noreply.github.com>
2023-07-18 14:19:37 +0300

    router, fn Error, rest api

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,62 @@
-// const fs = require('fs/promises')
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("models", "contacts.json");
 
-const getContactById = async (contactId) => {}
+const updateContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const allContacts = await listContacts();
+  const result = allContacts.find((contact) => contact.id === contactId);
+  return result || null;
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) return null;
 
-module.exports = {
+  const [result] = allContacts.splice(index, 1);
+  await updateContacts(allContacts);
+  return result;
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const allContacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    phone,
+    email,
+  };
+
+  allContacts.push(newContact);
+  await updateContacts(allContacts);
+  return newContact;
+};
+
+const updateContact = async (id, { name, email, phone }) => {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex((contact) => contact.id === id);
+
+  if (index === -1) return null;
+
+  allContacts[index] = { id, name, email, phone };
+  await updateContacts(allContacts);
+  return allContacts[index];
+};
+
+export default {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-}
+};
